06601f7e01f10b2ba6a5aabcdffaf7542dc7765d
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-30 21:26:30 -0600

    practicing react

---
+++ react-tutorial/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Table from './Table'
+
+class App extends Component {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        // the rest of the data
+      },
+    ],
+  }
+}
+
+removeCharacter = index => {
+  const { characters } = this.state
+
+  this.setState({
+    characters: characters.filter((character, i) => {
+      return i !== index
+    }),
+  })
+}
+
+render(); {
+  const { characters } = this.state
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={this.removeCharacter} />
+    </div>
+  )
+}
+
+export default App
